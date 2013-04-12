5a2f49e86ab82ab42f1f87aaa435b1cd7445ff7d
maxbpro <maxbpro2009@gmail.com>
2013-04-12 19:13:58 +0400

    created maven project

---
+++ StretchHeaderListVIew/StretchHeaderListView/src/main/java/com/maxb/pro/StretchHeaderListView/MainActivity.java
@@ -0,0 +1,22 @@
+package com.maxb.pro.StretchHeaderListView;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends Activity
+{
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+
+
+
+}
